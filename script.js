f22e5408e0cd1fa529acdc2e5ac9c6559c12a2bd
Erick <erickrossiumeki@gmail.com>
2026-01-13 13:26:15 -0300

    itens

--- script.js
+++ script.js
@@ -10,13 +10,36 @@ limparTabela();
     }
 });
 
-const links = document.querySelectorAll("#navbar a");
+const links = document.querySelectorAll('#navbar a');
+
+const notas = document.getElementById('notas');
+const itens = document.getElementById('itens');
+const desdob = document.getElementById('desdobramentos');
 
 links.forEach(link => {
-    link.addEventListener("click", () => {
-        links.forEach(l => l.classList.remove("ativo"));
-        link.classList.add("ativo");
-        
+    link.addEventListener('click', () => {
+
+        // ativa o link
+        links.forEach(l => l.classList.remove('ativo'));
+        link.classList.add('ativo');
+
+        // volta todos para o id base
+        notas.id = 'notas';
+        itens.id = 'itens';
+        desdob.id = 'desdobramentos';
+
+        // altera o id conforme o clique
+        if (link.textContent === 'Notas') {
+            notas.id = 'notas_visivel';
+        }
+
+        if (link.textContent === 'Itens') {
+            itens.id = 'itens_visivel';
+        }
+
+        if (link.textContent === 'Desdobramentos') {
+            desdob.id = 'desdobramentos_visivel';
+        }
     });
 });
 
@@ -89,7 +112,6 @@ function extrairDados(xml, nomeArquivo) {
     
     const dados = {
         arquivo: nomeArquivo,
-
         // Dados principais
         chave: xml.querySelector("chNFe")?.textContent || "",
         numero: xml.querySelector("nNF")?.textContent || "",
@@ -97,25 +119,88 @@ function extrairDados(xml, nomeArquivo) {
         emitente: xml.querySelector("emit > xNome")?.textContent || "",
         uf: xml.querySelector("emit > enderEmit > UF")?.textContent || "",
         valor: xml.querySelector("vNF")?.textContent || "0",
-
-        bs_icms: xml.querySelector("vBC")?.textContent || "0",
-        v_icms: xml.querySelector("vICMS")?.textContent || "0",
-
-        v_pis: xml.querySelector("vPIS")?.textContent || "0",
-
-        v_cofins: xml.querySelector("vCOFINS")?.textContent || "0",
-
-        desconto: xml.querySelector("vDesc")?.textContent || "0",
-        outras: xml.querySelector("vOutro")?.textContent || "0",
-        frete: xml.querySelector("vFrete")?.textContent || "0"
+        bs_icms: xml.querySelector("total > ICMSTot > vBC")?.textContent || "0",
+        v_icms: xml.querySelector("total > ICMSTot > vICMS")?.textContent || "0",
+        v_pis: xml.querySelector("total > ICMSTot > vPIS")?.textContent || "0",
+        v_cofins: xml.querySelector("total > ICMSTot > vCOFINS")?.textContent || "0",
+        desconto: xml.querySelector("total > ICMSTot > vDesc")?.textContent || "0",
+        outras: xml.querySelector("total > ICMSTot > vOutro")?.textContent || "0",
+        frete: xml.querySelector("total > ICMSTot > vFrete")?.textContent || "0"
     };
+    const produtos = [];
+    xml.querySelectorAll("det").forEach(det =>{
+        const produto = {
+        arquivo: nomeArquivo,
+        codigo: det.querySelector("prod > cProd")?.textContent || "",
+        descricao: det.querySelector("prod > xProd")?.textContent || "",
+        NCM: det.querySelector("prod > NCM")?.textContent || "",
+        vProd: det.querySelector("prod > vProd")?.textContent || "",
+        CFOP: det.querySelector("prod > CFOP")?.textContent || "",
+        icmscst: det.querySelector("ICMS CST")?.textContent || "",
+        aliqicms: det.querySelector("ICMS pICMS")?.textContent || "",
+        bsicms: det.querySelector("ICMS vBC")?.textContent || "",
+        vicms: det.querySelector("ICMS vICMS")?.textContent || "",
+        cstpis: det.querySelector("PIS CST")?.textContent || "",
+        aliqpis: det.querySelector("PIS pPIS")?.textContent || "",
+        bspis: det.querySelector("PIS vBC")?.textContent || "",
+        vpis: det.querySelector("PIS vPIS")?.textContent || "",
+        cstcofins: det.querySelector("COFINS CST")?.textContent || "",
+        aliqcofins: det.querySelector("COFINS pCOFINS")?.textContent || "",
+        bscofins: det.querySelector("COFINS vBC")?.textContent || "",
+        vcofins: det.querySelector("COFINS vCOFINS")?.textContent || "",
+        pdesconto: det.querySelector("prod > vDesc")?.textContent || "",
+        poutras: det.querySelector("prod > vOutro")?.textContent || "",
+        pfrete: det.querySelector("prod > vDesc")?.textContent || "",
+        };
+        produtos.push(produto);
+    });
+
     adicionarNaTabela(dados);
+    adicionarItens(produtos);
 }
 function limparTabela(){
     const tbody = document.querySelector("#tabela_xml tbody");
     while(tbody.firstChild){
         tbody.removeChild(tbody.firstChild)
     }
+    const tbodyi = document.querySelector("#tabela_itens tbody");
+    while(tbodyi.firstChild){
+        tbodyi.removeChild(tbodyi.firstChild)
+    }
+}
+function adicionarItens(produtos){
+    const tbody = document.querySelector("#tabela_itens tbody");
+
+    produtos.forEach(i => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td class="arq">${i.arquivo}</td>
+            <td>${i.codigo}</td>
+            <td>${i.descricao}</td>
+            <td>${i.NCM}</td>
+            <td>R$ ${Number(i.vProd).toFixed(2)}</td>
+            <td>${i.CFOP}</td>
+
+            <td class="icms">${i.icmscst}</td>
+            <td class="icms">${i.aliqicms}</td>
+            <td class="icms">R$ ${Number(i.bsicms).toFixed(2)}</td>
+            <td class="icms">R$ ${Number(i.vicms).toFixed(2)}</td>
+
+            <td class="pis">${i.cstpis}</td>
+            <td class="pis">${i.aliqpis}</td>
+            <td class="pis">R$ ${Number(i.bspis).toFixed(2)}</td>
+            <td class="pis">R$ ${Number(i.vpis).toFixed(2)}</td>
+
+            <td class="cofins">${i.cstcofins}</td>
+            <td class="cofins">${i.aliqcofins}</td>
+            <td class="cofins">R$ ${Number(i.bscofins).toFixed(2)}</td>
+            <td class="cofins">R$ ${Number(i.vcofins).toFixed(2)}</td>
+
+            <td>R$ ${Number(i.pdesconto).toFixed(2)}</td>
+            <td>R$ ${Number(i.poutras).toFixed(2)}</td>
+        `;
+        tbody.appendChild(tr);
+    });
 }
 function adicionarNaTabela(d) {
     const tbody = document.querySelector("#tabela_xml tbody");
